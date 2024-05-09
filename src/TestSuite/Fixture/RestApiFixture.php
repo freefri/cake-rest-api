f3e4ec8942ea251891d9792038e6ecd8754b9361
Adrian <freefri@freefri.es>
2024-05-09 10:23:59 +0200

    add restApiFixture to automatically add table prefix removing duplicated

--- src/TestSuite/Fixture/RestApiFixture.php
+++ src/TestSuite/Fixture/RestApiFixture.php
@@ -1,16 +1,21 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace RestApi\TestSuite\Fixture;
 
-use App\Model\Table\AppTable;
 use Cake\TestSuite\Fixture\TestFixture;
+use RestApi\Lib\RestPlugin;
 
 class RestApiFixture extends TestFixture
 {
     public function __construct()
     {
         if ($this->table === null) {
-            $this->table = AppTable::TABLE_PREFIX . $this->_tableFromClass();
+            $className = get_called_class();
+            $namespace = explode('\\', $className)[0] ?? '';
+            $tablePrefix = RestPlugin::getTablePrefixGeneric($namespace);
+            $this->table = $tablePrefix . $this->_tableFromClass();
         }
         parent::__construct();
     }
