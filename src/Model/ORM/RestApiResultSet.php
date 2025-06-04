65a52d21ea21d0c68fd7fbe5f79e14b1b95ad93a
Pablo <pablo@nordev.es>
2025-06-04 12:17:49 +0200

    copy exact version from ResultSetFactory

--- src/Model/ORM/RestApiResultSet.php
+++ src/Model/ORM/RestApiResultSet.php
@@ -1,34 +1,36 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace RestApi\Model\ORM;
 
-use Cake\Database\StatementInterface;
-use Cake\ORM\Query;
-use Cake\ORM\ResultSet;
+use Cake\Collection\Collection;
+use Cake\Datasource\ResultSetInterface;
+use Cake\ORM\Query\SelectQuery;
+use Cake\ORM\ResultSetFactory;
+use SplFixedArray;
 
-class RestApiResultSet extends ResultSet
+class RestApiResultSet extends ResultSetFactory
 {
-    /** @var InheritanceMarker */
-    public $inheritanceMarker;
-
-    public function __construct(Query $query, StatementInterface $statement)
+    public function createResultSet(iterable $results, ?SelectQuery $query = null): ResultSetInterface
     {
-        parent::__construct($query, $statement);
-        $repo = $query->getRepository();
-        if (property_exists($repo, 'inheritanceMarker')) {
-            $this->inheritanceMarker = $repo->inheritanceMarker;
-        }
+        if ($query) {
+            $data = $this->collectData($query);
 
-    }
-    protected function _groupResult(array $row)
-    {
-        if ($this->inheritanceMarker) {
-            $markerField = $this->_defaultAlias . '__' . $this->inheritanceMarker->getMarkerField();
-            if (isset($row[$markerField])) {
-                $this->_entityClass = $this->inheritanceMarker->getClassByType($row[$markerField]);
+            if (is_array($results)) {
+                foreach ($results as $i => $row) {
+                    $results[$i] = $this->groupResult($row, $data);
+                }
+
+                $results = SplFixedArray::fromArray($results);
+            } else {
+                $results = (new Collection($results))
+                    ->map(function ($row) use ($data) {
+                        return $this->groupResult($row, $data);
+                    });
             }
-
         }
-        return parent::_groupResult($row);
+
+        return new $this->resultSetClass($results);
     }
 }
