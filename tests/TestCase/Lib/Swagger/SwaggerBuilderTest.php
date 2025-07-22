21f4b0cc96d021a75dba270f3816560bd94923a1
Adrian <freefri@freefri.es>
2025-07-22 18:14:43 +0200

    fix tests

--- tests/TestCase/Lib/Swagger/SwaggerBuilderTest.php
+++ tests/TestCase/Lib/Swagger/SwaggerBuilderTest.php
@@ -337,7 +337,7 @@ class SwaggerBuilderTest extends TestCase
                                     'oneOf' => [
                                         [
                                             'type' => 'object',
-                                            'description' => 'Run bare',
+                                            'description' => 'Generic object when: Run bare',
                                             'properties' => [
                                                 'first' => [
                                                     'type' => 'string',
@@ -347,7 +347,148 @@ class SwaggerBuilderTest extends TestCase
                                         ],
                                         [
                                             'type' => 'object',
-                                            'description' => 'Run bare',
+                                            'description' => 'Generic object when: Run bare',
+                                            'properties' => [
+                                                'first' => [
+                                                    'type' => 'string',
+                                                    'example' => 'another_posted_body',
+                                                ],
+                                                'second' => [
+                                                    'type' => 'string',
+                                                    'example' => 'second_posted_body',
+                                                ],
+                                            ],
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                    'security' => [
+                        (int) 0 => [
+                            'bearerAuth' => []
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        $this->assertEquals(['paths' => $expected, 'componentSchemas' => []], $array);
+    }
+
+    public function testToArray_addingMultipleRequestWithDifferentParamsShouldSumarizeTheParamsAlsoInRequestBody()
+    {
+        $swagger = new SwaggerFromController();
+        $request = new ServerRequest();
+        $controller = new Controller($request, 'Pets');
+        $request1 = [
+            'url' => '/testurl/3',
+            'session' => null,
+            'query' => [],
+            'files' => [],
+            'environment' => [
+                'REQUEST_METHOD' => 'PATCH',
+                'QUERY_STRING' => '',
+                'REQUEST_URI' => '/testurl/3'
+            ],
+            'post' => [
+                'first' => 'posted_body'
+            ],
+            'cookies' => []
+        ];
+        $request2 = $request1;
+        $request2['query'] = ['my_param' => 'param_value'];
+        $request2['post'] = [
+            'first' => 'another_posted_body',
+            'second' => 'second_posted_body',
+        ];
+        $body = [
+            'hello' => 'world',
+        ];
+        $res = $this->_getResponse($body, 200);
+        $swagger->addToSwagger($controller, $request1, $res);
+        $swagger->addToSwagger($controller, $request2, $res);
+
+        $builder = new SwaggerBuilder($swagger);
+        $array = $builder->toArray();
+        $expected = [
+            '' => [
+                'patch' => [
+                    'operationId' => 'patchPets',
+                    'summary' => '',
+                    'description' => 'Run bare',
+                    'parameters' => [
+                        [
+                            'description' => 'Auth token',
+                            'in' => 'header',
+                            'name' => 'Authentication',
+                            'example' => 'Bearer ****************',
+                            'required' => true,
+                            'schema' => [
+                                'type' => 'string'
+                            ]
+                        ],
+                        [
+                            'description' => $this->_getLangIso(),
+                            'in' => 'header',
+                            'name' => 'Accept-Language',
+                            'example' => 'en',
+                            'required' => false,
+                            'schema' => [
+                                'type' => 'string'
+                            ]
+                        ],
+                        [
+                            'description' => '',
+                            'in' => 'query',
+                            'name' => 'my_param',
+                            'example' => 'param_value',
+                            'required' => false,
+                            'schema' => [
+                                'type' => 'string'
+                            ]
+                        ],
+                    ],
+                    'tags' => [
+                        (int) 0 => 'Pets'
+                    ],
+                    'responses' => [
+                        (int) 200 => [
+                            'description' => 'OK',
+                            'content' => [
+                                'application/json' => [
+                                    'schema' => [
+                                        'type' => 'object',
+                                        'description' => 'Generic object when: Run bare',
+                                        'properties' => [
+                                            'hello' => [
+                                                'type' => 'string',
+                                                'example' => 'world'
+                                            ]
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ],
+                    'requestBody' => [
+                        'description' => 'Request body can match to any of the 2 provided schemas',
+                        'content' => [
+                            'application/json' => [
+                                'schema' => [
+                                    'oneOf' => [
+                                        [
+                                            'type' => 'object',
+                                            'description' => 'Generic object when: Run bare',
+                                            'properties' => [
+                                                'first' => [
+                                                    'type' => 'string',
+                                                    'example' => 'posted_body',
+                                                ],
+                                            ],
+                                        ],
+                                        [
+                                            'type' => 'object',
+                                            'description' => 'Generic object when: Run bare',
                                             'properties' => [
                                                 'first' => [
                                                     'type' => 'string',
@@ -494,7 +635,7 @@ class SwaggerBuilderTest extends TestCase
                             'application/json' => [
                                 'schema' => [
                                     'type' => 'object',
-                                    'description' => 'Run bare',
+                                    'description' => 'Generic object when: Run bare',
                                     'properties' => [
                                         'first' => [
                                             'type' => 'string',
